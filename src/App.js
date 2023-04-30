dd12ae3ae5a9563cb53ac4409137d68d9ab1c760
Hardik <hardik.kotangale@gmail.com>
2023-05-01 02:03:35 +0530

    # with ' ' will be ignored, and an empty message aborts the commit.
     # On branch master
      # Changes to be committed:
     	modified:   package-lock.json
     	modified:   package.json
     	modified:   public/index.html
     	deleted:    public/logo192.png
     	deleted:    public/logo512.png
     	new file:   public/styles.css
     	deleted:    src/App.css
     	modified:   src/App.js
     	new file:   src/components/Start.js
     	new file:   src/components/Student/AddCoursesStd.js
     	new file:   src/components/Student/ChangePass.js
     	new file:   src/components/Student/CoursesStd.js
     	new file:   src/components/Student/HomeStudent.js
     	new file:   src/components/Student/LoginStudent.js
     	new file:   src/components/Student/NavbarElements.js
     	new file:   src/components/Student/PortalHeader.js
     	new file:   src/components/Student/Userprofile.js
     	new file:   src/components/Student/css/Courses.css
     	new file:   src/components/Student/css/PoratalBody.css
     	new file:   src/components/Student/css/studentPortal.css
     	new file:   src/components/Student/images/Oytie.png
     	new file:   src/components/Student/images/call.png
     	new file:   src/components/Student/images/email.png
     	new file:   src/components/Student/images/facebook.png
     	new file:   src/components/Student/images/instagram.png
     	new file:   src/components/Student/images/linkedin.png
     	new file:   src/components/Student/images/userlogo.png
     	new file:   src/components/Student/images/whatsapp.png
     	new file:   src/components/Student/images/youtube.png
     	new file:   src/firebase.js
     	new file:   src/img/Oytie-Blue.png
     	new file:   src/img/Oytie.png
     	deleted:    src/logo.svg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter,Routes,Route} from 'react-router-dom';
+import "react-toastify/dist/ReactToastify.css"
+import HomeStudent from './components/Student/HomeStudent';
+import LoginStudent from './components/Student/LoginStudent';
+import Start from './components/Start';
+import UserProfile from './components/Student/Userprofile';
+import PortalHeader from './components/Student/PortalHeader';
+import ChangePass from './components/Student/ChangePass';
+import CoursesStd from './components/Student/CoursesStd';
+import AddCoursesStd from './components/Student/AddCoursesStd';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <BrowserRouter>
+    <div className='App'>
+    <Routes>
+      <Route exact path="/HomeStudent" element={<HomeStudent/>}/>
+      <Route exact path="/LoginStudent" element={<LoginStudent/>}/>
+      <Route exact path="/UserProfile/:id" element={<UserProfile/>}/>
+      <Route exact path="/PortalHeader" element={<PortalHeader/>}/>
+      <Route exact path="/ChangePass/:id" element={<ChangePass/>}/>
+      <Route exact path="/AddCoursesStd/:id" element={<AddCoursesStd/>}/>
+      <Route exact path="/CoursesStd/:id" element={<CoursesStd/>}/>
+      <Route exact path="/" element={<Start/>}/>
+
+
+
+      
+      </Routes>
     </div>
+    </BrowserRouter>
+    
+    
+
+  
   );
 }
 
